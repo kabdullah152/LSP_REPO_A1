4e1f9ddad87e61ba280ef43851936d12ebfe731b
kabdullah152 <132641628+kabdullah152@users.noreply.github.com>
2024-10-15 11:36:18 -0400

    Revert "Midterm questions placeholder - HelloWorld file"
    This reverts commit 091b7801b3724d3929e13893036a9766710a96dd.

--- large_scale_programming/src/org/howard/edu/lsp/midterm/question1/HelloWorld.java
+++
@@ -1,7 +0,0 @@
-package org.howard.edu.lsp.midterm.question1;
-// Name: Kimora Abdullah
-public class HelloWorld {
-	public static void main(String[] args) {
-		System.out.println("Hello World!!");
-	}
-}
--- large_scale_programming/src/org/howard/edu/lsp/midterm/question2/HelloWorld.java
+++
@@ -1,7 +0,0 @@
-package org.howard.edu.lsp.midterm.question2;
-// Name: Kimora Abdullah
-public class HelloWorld {
-	public static void main(String[] args) {
-		System.out.println("Hello World!!");
-	}
-}
