c1d9356588ef8027a8e54ec822c73a26485a5c35
kabdullah152 <132641628+kabdullah152@users.noreply.github.com>
2024-10-15 11:43:27 -0400

    Hello World Java Files

---
+++ large_scale_programming/src/org/howard/edu/lsp/midterm/question4/HelloWorld.java
@@ -0,0 +1,7 @@
+package org.howard.edu.lsp.midterm.question4;
+// Name: Kimora Abdullah
+public class HelloWorld {
+	public static void main(String[] args) {
+		System.out.println("Hello World!!");
+	}
+}
