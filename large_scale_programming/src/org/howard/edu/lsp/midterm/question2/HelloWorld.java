091b7801b3724d3929e13893036a9766710a96dd
kabdullah152 <132641628+kabdullah152@users.noreply.github.com>
2024-10-15 11:24:28 -0400

    Midterm questions placeholder - HelloWorld file

---
+++ large_scale_programming/src/org/howard/edu/lsp/midterm/question2/HelloWorld.java
@@ -0,0 +1,7 @@
+package org.howard.edu.lsp.midterm.question2;
+// Name: Kimora Abdullah
+public class HelloWorld {
+	public static void main(String[] args) {
+		System.out.println("Hello World!!");
+	}
+}
---
+++ large_scale_programming/src/org/howard/edu/lsp/midterm/question3/HelloWorld.java
@@ -0,0 +1,7 @@
+package org.howard.edu.lsp.midterm.question3;
+// Name: Kimora Abdullah
+public class HelloWorld {
+	public static void main(String[] args) {
+		System.out.println("Hello World!!");
+	}
+}
