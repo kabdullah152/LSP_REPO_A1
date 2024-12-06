09b62654f8afa66daf386dbdc10e60e16d3f7b72
kabdullah152 <132641628+kabdullah152@users.noreply.github.com>
2024-12-05 21:01:33 -0500

    Create Triangle.java

---
+++ large_scale_programming/src/org/howard/edu/lsp/finalexam/question3/Triangle.java
@@ -0,0 +1,12 @@
+package org.howard.edu.lsp.finalexam.question3;
+
+//Triangle.java
+/**
+* Class representing a Triangle.
+*/
+public class Triangle implements Shape {
+ @Override
+ public void draw() {
+     System.out.println("Drawing a Triangle");
+ }
+}
