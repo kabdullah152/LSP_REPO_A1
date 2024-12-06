4409824d859a85b5d67d94b385ed7121e1cc729c
kabdullah152 <132641628+kabdullah152@users.noreply.github.com>
2024-12-05 20:58:49 -0500

    Create Circle.java

---
+++ large_scale_programming/src/org/howard/edu/lsp/finalexam/question3/Circle.java
@@ -0,0 +1,12 @@
+package org.howard.edu.lsp.finalexam.question3;
+
+//Circle.java
+/**
+* Class representing a Circle.
+*/
+public class Circle implements Shape {
+ @Override
+ public void draw() {
+     System.out.println("Drawing a Circle");
+ }
+}
